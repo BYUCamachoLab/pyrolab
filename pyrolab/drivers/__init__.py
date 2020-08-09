3d1a636eb29d626b129d214ab9b1f66906a9c82c
sequoiap <sequoiap4@gmail.com>
2020-08-09 11:19:21 -0600

    Lasers

---
+++ pyrolab/drivers/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © PyroLab Project Contributors
+# Licensed under the terms of the GNU GPLv3+ License
+# (see pyrolab/__init__.py for details)
+
+"""
+Drivers
+-------
+
+Submodule containing drivers for each supported instrument type.
+"""
+
+class Instrument:
+    pass
