87b0d4a1b395f24c2a0eb6a543acc05c82bd8ed0
Sequoia Ploeg <sequoiap4@gmail.com>
2020-08-13 15:31:29 -0600

    One-stop api shop

---
+++ pyrolab/api.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Sequoia Ploeg
+# Licensed under the terms of the MIT License
+# (see pyrolab/__init__.py for details)
+
+"""
+API
+---
+
+...
+"""
+
+
+from Pyro5.core import locate_ns
+from Pyro5.server import expose
+
+
+
+__all__ = [
+    "locate_ns", "expose",
+]
