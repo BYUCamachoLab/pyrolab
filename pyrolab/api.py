62bc2a1c2b47bebcc6bbc9ac5146de9924854381
Sequoia Ploeg <sequoiap4@gmail.com>
2020-08-14 15:21:41 -0600

    More useful pyro functions

--- pyrolab/api.py
+++ pyrolab/api.py
@@ -14,7 +14,8 @@ A single module that centralizes the most frequently used objects from PyroLab.
 from pyrolab.configure import global_config as config
 from Pyro5.core import locate_ns
 from pyrolab.client import Proxy, CertCheckingProxy
-from pyrolab.server import Daemon, CertValidatingDaemon, expose
+from pyrolab.server import Daemon, CertValidatingDaemon
+from Pyro5.server import expose, behavior, oneway, serve
 from pyrolab.nameserver import start_ns_loop
 
 
@@ -27,5 +28,8 @@ __all__ = [
     "Daemon",
     "CertValidatingDaemon", 
     "expose",
+    "behavior",
+    "oneway",
+    "serve",
     "start_ns_loop",
 ]
