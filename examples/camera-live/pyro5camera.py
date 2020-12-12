6e0a2618086372d8e83e9f364f7951388bc6784f
hillda3141 <dah3141@byu.net>
2020-12-12 13:26:13 -0700

    server functioning at very low speed, but getting good image

--- examples/camera-live/pyro5camera.py
+++ examples/camera-live/pyro5camera.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:examples/camera-live/pyro5camera.py
 from pyrolab.api import locate_ns, Proxy
-=======
->>>>>>> 5ef3a154226189edc07e07e6be614bacb7185e44:examples/camera-live/cameratest.py
-from pyrolab.drivers.cameras import uc480 as cam
 import numpy as np
 import cv2
 import sys
@@ -29,13 +25,9 @@ def bayer_convert(bayer):
     dStack = np.dstack((B,G,R))
     return dStack
 
-<<<<<<< HEAD:examples/camera-live/pyro5camera.py
 ns = locate_ns(host="camacholab.ee.byu.edu")
 cam = Proxy(ns.lookup("UC480"))
 
-=======
-cam = cam.UC480()
->>>>>>> 5ef3a154226189edc07e07e6be614bacb7185e44:examples/camera-live/cameratest.py
 cam.open()
 cam.set_pixel_clock(24)
 cam.set_color_mode(mode=11)
@@ -46,25 +38,16 @@ cam.set_exposure(90)
 
 cam.initialize_memory(pixelbytes=8)
 cam.start_capture(1)
-<<<<<<< HEAD:examples/camera-live/pyro5camera.py
-
-while(True):
-    bayer = np.array(cam.get_image(), dtype=np.uint8).reshape(1024, 1280)
-    print(bayer)
-    dStack = bayer_convert(bayer)
-
-=======
 
 while(True):
     #bayer = cam.get_image()
     bayer = np.array(cam.get_image(), dtype=np.uint8).reshape(1024, 1280)
     #print(bayer)
     dStack = bayer_convert(bayer)
-    print(dStack)
+    #print(dStack)
 
->>>>>>> 5ef3a154226189edc07e07e6be614bacb7185e44:examples/camera-live/cameratest.py
     cv2.imshow('scope',dStack)
-    keyCode = cv2.waitKey(10)
+    keyCode = cv2.waitKey(1)
     if cv2.getWindowProperty('scope',cv2.WND_PROP_VISIBLE) < 1:        
         break
 
