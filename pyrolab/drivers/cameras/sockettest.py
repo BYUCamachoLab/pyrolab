cb6169b27f927d76edba0d488f107a09a3fd1d1d
David Andrew Hill <dah3141@et.byu.edu>
2021-01-14 17:09:41 -0700

    with socket test

---
+++ pyrolab/drivers/cameras/sockettest.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 2222))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established")
+    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    clientsocket.close()
