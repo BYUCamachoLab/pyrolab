0e33f4e86336c6b5384abb819d67b56b8c8fad29
David Andrew Hill <dah3141@et.byu.edu>
2021-01-19 09:14:24 -0700

    changed

--- pyrolab/drivers/firmata/lamp.py
+++ pyrolab/drivers/firmata/lamp.py
@@ -11,43 +11,22 @@ Author: David Hill (https://github.com/hillda3141)
 Repo: https://github.com/BYUCamachoLab/pyrolab/blob/bpc303/pyrolab/drivers/motion
 """
 
-from win32event import CreateMutex
-from win32api import GetLastError
-from winerror import ERROR_ALREADY_EXISTS
-from sys import exit
-
-handle = CreateMutex(None, 1, 'ARD_LAMP')
-
 import pyfirmata
 import time
-
-if GetLastError() == ERROR_ALREADY_EXISTS:
-    # Take appropriate action, as this is the second instance of this script.
-    print('An instance of this application is already running.')
-    exit(1)
-
-from Pyro5.api import expose, locate_ns, Daemon, config, behavior
-
-def get_ip():
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
-
+from Pyro5.api import expose
 import pyrolab.api
 
 @expose
-class ARD_LAMP:
+class LAMP:
 
-    def __init__(self, port="COM5"):
+    def __init__(self,port="COM5"):
+        self.port = port
         pass
 
-    def set_port(self,port='COM5'):
+    def set_port(self,port="COM5"):
         self.port = port
 
-    def start_serial(self):
+    def start(self):
         self.board = pyfirmata.Arduino(self.port)
 
     def on(self,pin=13):
@@ -55,21 +34,3 @@ class ARD_LAMP:
 
     def off(self,pin=13):
         self.board.digital[pin].write(0)
-
-    def end(self):
-        for x in range(2,13):
-            self.board.digital[x].write(0)
-
-
-if __name__ == "__main__":
-    config.HOST = get_ip()
-    config.SERVERTYPE = "multiplex"
-    daemon = Daemon()
-    ns = locate_ns(host="camacholab.ee.byu.edu")
-
-    uri = daemon.register(ARD_LAMP)
-    ns.register("ARD_LAMP", uri)
-    try:
-        daemon.requestLoop()
-    finally:
-        ns.remove("ARD_LAMP")
