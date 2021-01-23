386af37372ce8ed338b2cc48a8a3c61697ec8493
hillda3141 <dah3141@byu.net>
2021-01-23 13:36:19 -0700

    edit

--- pyrolab/drivers/lasers/ppcl550.py
+++ pyrolab/drivers/lasers/ppcl550.py
@@ -22,275 +22,157 @@ import threading
 
 from Pyro5.api import expose
 
-global ready
-ready = True
-global start
-start = False
-global clientsocket
-clientsocket  = None
-
 import pyrolab.api
 
+ITLA_NOERROR=0x00
+ITLA_EXERROR=0x01
+ITLA_AEERROR=0x02
+ITLA_CPERROR=0x03
+ITLA_NRERROR=0x04
+ITLA_CSERROR=0x05
+ITLA_ERROR_SERPORT=0x01
+ITLA_ERROR_SERBAUD=0x02
+
+REG_Nop=0x00
+REG_Mfgr=0x02
+REG_Model=0x03
+REG_Serial=0x04
+REG_Release=0x06
+REG_Gencfg=0x08
+REG_AeaEar=0x0B
+REG_Iocap=0x0D
+REG_Ear=0x10
+REG_Dlconfig=0x14
+REG_Dlstatus=0x15
+REG_Channel=0x30
+REG_Power=0x31
+REG_Resena=0x32
+REG_Grid=0x34
+REG_Fcf1=0x35
+REG_Fcf2=0x36
+REG_Oop=0x42
+REG_Opsl=0x50
+REG_Opsh=0x51
+REG_Lfl1=0x52
+REG_Lfl2=0x53
+REG_Lfh1=0x54
+REG_Lfh2=0x55
+REG_Currents=0x57
+REG_Temps=0x58
+REG_Ftf=0x62
+REG_Mode=0x90
+REG_PW=0xE0
+REG_Csweepsena=0xE5
+REG_Csweepamp=0xE4
+REG_Cscanamp=0xE4
+REG_Cscanon=0xE5
+REG_Csweepon=0xE5
+REG_Csweepoffset=0xE6
+REG_Cscanoffset=0xE6
+REG_Cscansled=0xF0
+REG_Cscanf1=0xF1
+REG_Cscanf2=0xF2
+REG_CjumpTHz=0xEA
+REG_CjumpGHz=0xEB
+REG_CjumpSled=0xEC
+REG_Cjumpon=0xED
+REG_Cjumpoffset=0xE6
+
+READ=0
+WRITE=1
+
 @expose
-class UC480:
+class PPCL550:
 
-    """ A Thorlabs UC480 Camera.
-    This assumes there is only one camera connected to the computer.
-    ----------
-    handle : handle
-        c-type handle to reference camera (serial port, etc)
-    camera: str
-        camera name
-    bit_depth : int
-        the number of bits used for each pixel (usually is 8)
-    meminfo : array(int)
-        points to the memory location where captured frames are stored
-    roi_shape : array(int)
-        dimentions of the image that is taken by the camera (usually 1024 x 1280)
-    roi_pos : array(int)
-        position of the top left corner of the roi (region of interest) in relation to the sensor array (usaually 0,0)
-    framerate : int
-        the framerate of the camera in frames per second
-    video_thread : thread
-        seperate thread used to run parrell to the main loop that streams the video fromt he camera
-    stop_video : threading.Event (similar to boolean)
-        this is used to end the video streaming by ending the parrellel "video_thread"
-    serversocket : socket
-        socket that represents the server computer (running on port 2222)
-    clientsocket : socket
-        socket that represents the client computer
-    start_socket : boolean
-        if true, initiates the socket server connection
-    Attributes
-    ----------
-    HEADERSIZE : int
-        the size of the header file used to communicate the size of the message (10 bytes is a safe size)
-    """
+    latestregister=0
+    tempport=0
+    raybin=0
+    queue=[]
+    maxrowticket=0
 
-    handle = None
-    camera = None
-    bit_depth = None
-    meminfo = None
-    roi_shape = None
-    roi_pos = None
-    framerate = None
-    video_thread = None
-    stop_video = None
-    serversocket = None
-    clientsocket = None
-    start_socket = None
+    _error=ITLA_NOERROR
+    seriallock = 0
+
+    def connect(port,baudrate=9600):
+        reftime=time.clock()
+        connected=False
+        try:
+            conn = serial.Serial(port,baudrate,timeout=1)
+        except serial.SerialException:
+            _error = ITLA_ERROR_SERPORT
+            return(ITLA_ERROR_SERPORT)
+        baudrate2=4800
+        while baudrate2<115200:
+            self.send(conn,REG_Nop,0,0)
+            if ITLALastError() != ITLA_NOERROR:
+                #go to next baudrate
+                if baudrate2==4800:baudrate2=9600
+                elif baudrate2==9600: baudrate2=19200
+                elif baudrate2==19200: baudrate2=38400
+                elif baudrate2==38400:baudrate2=57600
+                elif baudrate2==57600:baudrate2=115200
+                conn.close()
+                conn = serial.Serial(port,baudrate2,timeout=1)            
+            else:
+                return(conn)
+        conn.close()
+        _error = ITLA_ERROR_SERBAUD
+        return(ITLA_ERROR_SERBAUD)
+
+    def communicate(sercon,register,data,rw):
+        lock = threading.Lock()
+        lock.acquire()
+        rowticket = self.maxrowticket + 1
+        maxrowticket = maxrowticket + 1
+        self.queue.append(rowticket)
+        lock.release()
+        while queue[0] != rowticket:
+            rowticket=rowticket
+        if rw == 0:
+            byte2 = int(data/256)
+            byte3 = int(data - byte2*256)
+            self.latestregister = register
+            msg = [0,register,byte2,byte3]
+            msg[0] = msg[0] & checksum(msg)
+            self.send(sercon,msg)
+            recvmsg = self.recieve(sercon)
+            datamsg = recvmsg[2]*256 + recvmsg[3]
+            if (recvmsg[0] & 0x03) == 0x02:
+                extmsg = self.AEA(sercon,datamsg)
+                lock.acquire()
+                queue.pop(0)
+                lock.release()
+                return extmsg
+            lock.acquire()
+            queue.pop(0)
+            lock.release()
+            return datamsg
+        else:
+            byte2=int(data/256)
+            byte3=int(data - byte2*256)
+            msg = [0,register,byte2,byte3]
+            msg[0] = msg[0] & checksum(msg)
+            self.send(sercon,msg)
+            recvmsg = self.recieve(sercon)
+            lock.acquire()
+            queue.pop(0)
+            lock.release()
+            datamsg = recvmsg[2]*256 + recvmsg[3]
+            return(datamsg)
+
+    def checksum(msg):
+        bip8=(msg[0] & 0x0f) ^ msg[1] ^ msg[2] ^ msg[3]
+        bip4=((bip8 & 0xf0) >> 4) ^ (bip8 & 0x0f)
+        return bip4
+
+    def SerialLock():
+        seriallock=1
+    
+    def SerialUnlock():
+        seriallock=0
+        queue.pop(0)
 
     def __init__(self):
         pass
 
-    def open(self, bit_depth=8, camera="ThorCam FS"):
-        """
-        Opens the serial communication with the Thorlabs camera and sets
-        some low-level values, including the bit depth and camera name.
-        """
-        print(tc.GetCameraList)
-        num = c_int(0)
-        tc.GetNumberOfCameras(byref(num))
-        print(num.value)
-        self.bit_depth = bit_depth
-        self.camera = camera
-        
-        self.handle = c_int(0)
-        i = tc.InitCamera(byref(self.handle))     
-        #print("Display Mode:")
-        tc.SetDisplayMode(self.handle, c_int(32768)) 
-        #print(i)
-        if i == 0:
-            print("ThorCam opened successfully.")
-        else:
-            print("Opening the ThorCam failed with error code "+str(i))
-
-    def close(self, waitMode):
-        """
-        Calls self.stop_capture to free memory and end the socket server
-        and then closes serial communication with the camera.
-        """
-        if self.handle != None:
-            self.stop_capture(waitMode)
-            i = tc.ExitCamera(self.handle) 
-            if i == 0:
-                print("ThorCam closed successfully.")
-            else:
-                print("Closing ThorCam failed with error code "+str(i))
-        else:
-            return
-
-    def _get_image(self):
-        """
-        Retrieves the last frame from the memory buffer, processes
-        it into a gray-scale image, and serializes it using the pickle
-        library. The header is then added to inform the client how long
-        the message is. This should not be called from the client. It
-        will be called from the function _video_loop() which is on a
-        parrallel thread with Pyro5.
-        """
-        bayer = np.frombuffer(self.meminfo[0], c_ubyte).reshape(self.roi_shape[1], self.roi_shape[0])
-
-        ow = (bayer.shape[0]//4) * 4
-        oh = (bayer.shape[1]//4) * 4
-
-        R  = bayer[0::2, 0::2]
-        B  = bayer[1::2, 1::2]
-        G0 = bayer[0::2, 1::2]
-        G1 = bayer[1::2, 0::2]
-
-        GRAY = R[:oh,:ow]//3 + B[:oh,:ow]//3 + (G0[:oh,:ow]//2 + G1[:oh,:ow]//2)//3
-
-        #print(np.dstack((B,G,R)))
-
-        #print(im)
-        #image = GRAY.tolist()
-        msg = pickle.dumps(GRAY)
-        #print(len(msg))
-        msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
-        return msg
-
-    def _video_loop(self):
-        """
-        This function is called as a seperate thread when streaming is initiated.
-        It will loop, sending frame by frame accross the socket connection,
-        until the threading.Event() stop_video is triggered.
-        """
-        while not self.stop_video.is_set():
-            if self.start_socket==True:
-                while True:
-                    print("setting up server...")
-                    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self.serversocket.bind((socket.gethostname(), 2222))
-                    self.serversocket.listen(5)
-                    self.clientsocket, address = self.serversocket.accept()
-                    print("server set up")
-                    self.start_socket = False
-                    break
-            msg = self._get_image()
-            #print("image grabbed")
-            self.clientsocket.send(msg)
-            #print("message sent")
-            while True:
-                check_msg = self.clientsocket.recv(2)
-                break
-        
-    def set_pixel_clock(self, clockspeed):
-        """
-        Sets the clockspeed of the camera, usually in the range of 24.
-        """
-        pixelclock = c_uint(clockspeed)
-        i = tc.PixelClock(self.handle, 6, byref(pixelclock), sizeof(pixelclock))
-        print("Pixel:")
-        print(i)
-
-    def start_capture(self, waitMode):
-        """
-        This starts the capture from the camera to the allocated
-        memory location as well as starts a new parallel thread
-        for the socket server to stream from memory to the client.
-        """
-        tc.StartCapture(self.handle, waitMode)
-        self.start_socket = True
-        self.stop_video = threading.Event()
-        self.video_thread = threading.Thread(target=self._video_loop, args=())
-        self.video_thread.start()
-
-    def stop_capture(self, waitMode):
-        """
-        This frees the memory used for storing the frames then triggers
-        the stop_video event which will end the parrallel socket thread.
-        """
-        tc.FreeMemory(self.handle, self.meminfo[0], self.meminfo[1])
-        #self.epix.pxd_goUnLive(0x1)
-        tc.StopCapture(self.handle, waitMode)
-        self.clientsocket.close()
-        self.stop_video.set()
-        print("unlive now")
-        
-    def initialize_memory(self, pixelbytes=8):
-        """
-        Initializes the memory for holding the most recent frame from the camera.
-        """
-        if self.meminfo != None:
-            tc.FreeMemory(self.handle, self.meminfo[0], self.meminfo[1])
-        
-        xdim = self.roi_shape[0]
-        ydim = self.roi_shape[1]
-        #print(xdim)
-        #print(ydim)
-        imagesize = xdim*ydim
-        #print(imagesize)
-            
-        memid = c_int(0)
-        c_buf = (c_ubyte * imagesize)(0)
-        #print(c_buf)
-
-        tc.AllocateMemory(self.handle, xdim, ydim, c_int(pixelbytes), c_buf, byref(memid))
-        #print(c_buf)
-        #print(memid)
-        tc.SetImageMemory(self.handle, c_buf, memid)
-        self.meminfo = [c_buf, memid]
     
-    def set_exposure(self, exposure):
-        """
-        This sets the exposure of the camera in milliseconds (90 is a good exposure value).
-        """
-        exposure_c = c_double(exposure)
-        tc.SetExposure(self.handle, 12 , exposure_c, sizeof(exposure_c))
-        self.exposure = exposure_c.value
-    
-    def set_framerate(self, fps):
-        """
-        Sets the framerate of the camera (fps). After calling
-        this function you must reset the exposure.
-        """      
-        s_framerate = c_double(0)
-        tc.SetFrameRate(self.handle, c_double(fps), byref(s_framerate))
-        self.framerate = s_framerate.value
-
-    def set_color_mode(self, mode=11):
-        """
-        This sets the mode of image that is taken, almost always
-        use 11 which will give you the raw photosensor data in the format:
-            | R  G0 |...
-            | G1  B |...
-              .   .
-              .   .
-              .   .
-        This data is interpreted in the _get_image() function.
-        """
-        i = tc.SetColorMode(self.handle, mode) #11 means raw 8-bit, 6 means gray 8-bit
-        print("Color Mode:")
-        print(i)
-
-    def set_roi_shape(self, roi_shape):
-        """
-        Sets the dimmenstions of the region of interest (roi)
-        """
-        AOI_size = tc.IS_2D(roi_shape[0], roi_shape[1]) #Width and Height
-            
-        i = tc.AOI(self.handle, 5, byref(AOI_size), 8)#5 for setting size, 3 for setting position
-        tc.AOI(self.handle, 6, byref(AOI_size), 8)#6 for getting size, 4 for getting position
-        self.roi_shape = [AOI_size.s32X, AOI_size.s32Y]
-        print(self.roi_shape)
-        if i == 0:
-            print("ThorCam ROI size set successfully.")
-        else:
-            print("Set ThorCam ROI size failed with error code "+str(i))
-
-    def set_roi_pos(self, roi_pos):
-        """
-        Sets the origin position of the region of interest (usually (0,0))
-        """
-        AOI_pos = tc.IS_2D(roi_pos[0], roi_pos[1]) #Width and Height
-            
-        i = tc.AOI(self.handle, 3, byref(AOI_pos), 8 )#5 for setting size, 3 for setting position
-        tc.AOI(self.handle, 4, byref(AOI_pos), 8 )#6 for getting size, 4 for getting position
-        self.roi_pos = [AOI_pos.s32X, AOI_pos.s32Y]
-        print(self.roi_pos)
-        
-        if i == 0:
-            print("ThorCam ROI position set successfully.")
-        else:
-            print("Set ThorCam ROI pos failed with error code "+str(i))
