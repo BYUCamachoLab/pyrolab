37cc96aa452d011d23c9f77bf8c3138822de45b2
hillda3141 <dah3141@byu.net>
2021-01-23 17:43:12 -0700

    serial connection and other functions implemented

---
+++ examples/pp-laser/pplasertest.py
@@ -0,0 +1,20 @@
+from pyrolab.drivers.lasers import ppcl550 as ppl
+import time
+
+laser = ppl.PPCL550()
+back = laser.connect("COM6",baudrate=9600)
+print(back)
+back = laser.setPower(14)
+print(back)
+back = laser.setWavelength(1550)
+print(back)
+back = laser.setChannel(1)
+print(back)
+back = laser.start()
+print(back)
+time.sleep(5)
+back = laser.stop()
+print(back)
+back = laser.disconnect()
+print(back)
+
--- pyrolab/drivers/lasers/ppcl550.py
+++ pyrolab/drivers/lasers/ppcl550.py
@@ -24,6 +24,8 @@ from Pyro5.api import expose
 
 import pyrolab.api
 
+C_SPEED = 299792458
+
 ITLA_NOERROR=0x00
 ITLA_EXERROR=0x01
 ITLA_AEERROR=0x02
@@ -84,95 +86,154 @@ WRITE=1
 @expose
 class PPCL550:
 
-    latestregister=0
-    tempport=0
-    raybin=0
-    queue=[]
-    maxrowticket=0
+    latestregister = 0
+    tempport = 0
+    raybin = 0
+    queue = []
+    maxrowticket = 0
+    lasercom = serial.Serial()
 
     _error=ITLA_NOERROR
     seriallock = 0
+    
+    def __init__(self):
+        pass
 
-    def connect(port,baudrate=9600):
-        reftime=time.clock()
+    def connect(self,port,baudrate=9600):
+        reftime = time.time()
         connected=False
         try:
-            conn = serial.Serial(port,baudrate,timeout=1)
+            self.lasercom = serial.Serial(port,baudrate,timeout=1)
         except serial.SerialException:
-            _error = ITLA_ERROR_SERPORT
             return(ITLA_ERROR_SERPORT)
-        baudrate2=4800
-        while baudrate2<115200:
-            self.send(conn,REG_Nop,0,0)
-            if ITLALastError() != ITLA_NOERROR:
-                #go to next baudrate
-                if baudrate2==4800:baudrate2=9600
-                elif baudrate2==9600: baudrate2=19200
-                elif baudrate2==19200: baudrate2=38400
-                elif baudrate2==38400:baudrate2=57600
-                elif baudrate2==57600:baudrate2=115200
-                conn.close()
-                conn = serial.Serial(port,baudrate2,timeout=1)            
-            else:
-                return(conn)
-        conn.close()
-        _error = ITLA_ERROR_SERBAUD
-        return(ITLA_ERROR_SERBAUD)
+        back = self.communicate(REG_Nop,0,0)
+        print(back)
+        if back == ITLA_NOERROR:
+            return(ITLA_NOERROR)
+        else:
+            self.lasercom.close()
+            return(ITLA_ERROR_SERBAUD)
 
-    def communicate(sercon,register,data,rw):
+    def disconnect(self):
+        self.lasercom.close()
+        return 0
+
+    def setPower(self,power):
+        sendPower = int(power*100)
+        back = self.communicate(REG_Power,sendPower,1)
+        return back
+
+    def setChannel(self,channel=1):
+        back = self.communicate(REG_Channel,channel,1)
+        return back
+
+    def setWavelength(self,wavelength):
+        if(wavelength < 1515 or wavelength > 1625):
+            return "wavelength not in range"
+        else:
+            freq = self.wl_freq(wavelength)
+            freq_t = int(freq/1000)
+            freq_g = int(freq*10)
+            back = self.communicate(REG_Fcf1,freq_t,1)
+            if(back == ITLA_NOERROR):
+                back = self.communicate(REG_Fcf2,freq_g,1)
+            return back
+    
+    def start(self):
+        back = self.communicate(REG_Resena,8,1)
+        return back
+    
+    def stop(self):
+        back = self.communicate(REG_Resena,0,1)
+        return back
+
+    def communicate(self,register,data,rw):
         lock = threading.Lock()
         lock.acquire()
         rowticket = self.maxrowticket + 1
-        maxrowticket = maxrowticket + 1
+        self.maxrowticket = self.maxrowticket + 1
         self.queue.append(rowticket)
         lock.release()
-        while queue[0] != rowticket:
+        while self.queue[0] != rowticket:
             rowticket=rowticket
         if rw == 0:
             byte2 = int(data/256)
             byte3 = int(data - byte2*256)
             self.latestregister = register
             msg = [0,register,byte2,byte3]
-            msg[0] = msg[0] & checksum(msg)
-            self.send(sercon,msg)
-            recvmsg = self.recieve(sercon)
+            msg[0] = msg[0] & int(self.checksum(msg))*16
+            self.send(msg)
+            recvmsg = self.recieve()
+            print(recvmsg)
             datamsg = recvmsg[2]*256 + recvmsg[3]
             if (recvmsg[0] & 0x03) == 0x02:
-                extmsg = self.AEA(sercon,datamsg)
+                extmsg = self.AEA(datamsg)
                 lock.acquire()
-                queue.pop(0)
+                self.queue.pop(0)
                 lock.release()
                 return extmsg
             lock.acquire()
-            queue.pop(0)
+            self.queue.pop(0)
             lock.release()
-            return datamsg
+            errorMsg = int(recvmsg[0] & 0x03)
+            return(errorMsg)
         else:
             byte2=int(data/256)
             byte3=int(data - byte2*256)
             msg = [0,register,byte2,byte3]
-            msg[0] = msg[0] & checksum(msg)
-            self.send(sercon,msg)
-            recvmsg = self.recieve(sercon)
+            msg[0] = msg[0] & (int(self.checksum(msg))*16 + 1)
+            self.send(msg)
+            recvmsg = self.recieve()
             lock.acquire()
-            queue.pop(0)
+            self.queue.pop(0)
             lock.release()
-            datamsg = recvmsg[2]*256 + recvmsg[3]
-            return(datamsg)
+            errorMsg = int(recvmsg[0] & 0x03)
+            return(errorMsg)
 
-    def checksum(msg):
+    def send(self,msg):
+        print(f"Sent msg: {msg}")
+        self.lasercom.write(bytes(chr(msg[0]),"utf-8"))
+        self.lasercom.write(bytes(chr(msg[1]),"utf-8"))
+        self.lasercom.write(bytes(chr(msg[2]),"utf-8"))
+        self.lasercom.write(bytes(chr(msg[3]),"utf-8"))
+
+    def recieve(self):
+        reftime = time.time()
+        while self.lasercom.inWaiting()<4:
+            if(time.time() > reftime + 0.25):
+                self._error=ITLA_NRERROR
+                return(0xFF,0xFF,0xFF,0xFF)
+            time.sleep(0.0001)
+        try:
+            msg = []
+            msg.append(ord(self.lasercom.read(1)))
+            msg.append(ord(self.lasercom.read(1)))
+            msg.append(ord(self.lasercom.read(1)))
+            msg.append(ord(self.lasercom.read(1)))
+        except:
+            print(f"problem with serial communication. queue[0] = {self.queue[0]}")
+            msg = [0xFF,0xFF,0xFF,0xFF]
+        print(f"Recieved msg: {msg}")
+        if self.checksum(msg) == msg[0] >> 4:
+            print("good cs")
+            self._error = msg[0] & 0x03
+            return(msg)
+        else:
+            print("bad cs")
+            self._error=ITLA_CSERROR
+            return(msg)  
+
+    def checksum(self,msg):
         bip8=(msg[0] & 0x0f) ^ msg[1] ^ msg[2] ^ msg[3]
         bip4=((bip8 & 0xf0) >> 4) ^ (bip8 & 0x0f)
         return bip4
 
-    def SerialLock():
+    def wl_freq(self,unit):
+        return C_SPEED/unit
+
+    def SerialLock(self):
         seriallock=1
     
-    def SerialUnlock():
+    def SerialUnlock(self):
         seriallock=0
-        queue.pop(0)
-
-    def __init__(self):
-        pass
-
-    
+        self.queue.pop(0)    
