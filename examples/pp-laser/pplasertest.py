cb742c38b791e6adebbb86aaae97bb84819ac10a
David Andrew Hill <dah3141@et.byu.edu>
2021-01-26 20:52:46 -0700

    added client support via pyrolab

--- examples/pp-laser/pplasertest.py
+++ examples/pp-laser/pplasertest.py
@@ -1,20 +1,23 @@
-from pyrolab.drivers.lasers import ppcl550 as ppl
+from pyrolab.api import locate_ns, Proxy
+import cv2
+import sys
 import time
 
-laser = ppl.PPCL550()
+ns = locate_ns(host="camacholab.ee.byu.edu")
+laser = Proxy(ns.lookup("PPCL550"))
+
 back = laser.connect("COM4",baudrate=9600)
 print(back)
-back = laser.setPower(14)
+back = laser.setPower(10)
 print(back)
-back = laser.setWavelength(1550)
+back = laser.setWavelength(1590)
 print(back)
 back = laser.setChannel(1)
 print(back)
 back = laser.start()
 print(back)
-time.sleep(5)
+time.sleep(10)
 back = laser.stop()
 print(back)
 back = laser.disconnect()
 print(back)
-
