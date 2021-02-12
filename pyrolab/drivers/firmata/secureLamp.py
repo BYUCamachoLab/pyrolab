629eb838d722b3ae48f2c04cd184d5119877f02d
David Andrew Hill <dah3141@et.byu.edu>
2021-02-12 15:03:46 -0700

    made changes to wrapped lamp class

--- pyrolab/drivers/firmata/secureLamp.py
+++ pyrolab/drivers/firmata/secureLamp.py
@@ -1,18 +1,21 @@
-import lamp
+import pyrolab.drivers.firmata.lamp as lamp
 import os
 from os import path
+from Pyro5.api import expose
+import pyrolab.api
 
-class SECURE_LAMP(ARD_LAMP):
+@expose
+class SECURE_LAMP(lamp.LAMP):
 
-    def __init__():
+    def __init__(self,port):
         status = self.get_status()
         if(status == "unlocked"):
-            super.__init__()
-    def lock():
+            super.__init__(port)
+    def lock(self):
         f = open("LOCK_LAMP.txt","w+")
         f.close()
-    def release():
+    def release(self):
         os.remove("LOCK_LAMP.txt")
-    def get_status():
+    def get_status(self):
         x = os.path.exists("LOCK_LAMP.txt")
         print(x)
