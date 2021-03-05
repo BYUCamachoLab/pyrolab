b6b962d86c974e86b77bbdb79ebdfc6699cb0811
David Andrew Hill <dah3141@et.byu.edu>
2021-03-05 14:17:09 -0700

    added pythonic comments

--- pyrolab/drivers/firmata/lamp.py
+++ pyrolab/drivers/firmata/lamp.py
@@ -9,6 +9,15 @@
 Driver for the Arduino that is connected to a Thorlabs Microscope Lamp.
 Author: David Hill (https://github.com/hillda3141)
 Repo: https://github.com/BYUCamachoLab/pyrolab/blob/bpc303/pyrolab/drivers/motion
+
+Functions
+---------
+    __init__(self,port)
+    start(self)
+    on(self,pin=13)
+    off(self,pin=13)
+    close(self)
+    __del__(self)
 """
 
 import pyfirmata
@@ -21,10 +30,31 @@ import pyrolab.api
 class LAMP:
 
     def __init__(self,port):
+        """"
+        LAMP constructor.
+
+        Parameters
+        ----------
+        port : string
+            Port name that the arduino is connected to. Ex: "COM4"
+        """
         self._activated = True
         self.port = port
 
     def start(self):
+        """"
+        Initialize a connection with the arduino.
+
+        Parameters
+        ----------
+        self.port : string
+            Port name that is initialized in __init__(). Ex: "COM4"
+        
+        Raises
+        ------
+        PyroError("DeviceLockedError")
+            Error to signal that the constuctor was not called and therefore the device is locked.
+        """
         try:
             self._activated
         except AttributeError:
@@ -33,6 +63,21 @@ class LAMP:
         self.board = pyfirmata.Arduino(self.port)        
 
     def on(self,pin=13):
+        """"
+        Tell the arduino to turn a pin HIGH.
+
+        Parameters
+        ----------
+        self.board : pyfirmata.Arduino
+            Oject that represents an arduino initialized in start(). Ex: pyfirmata.Arduino("COM4")
+        pin : int
+            Integer that represents the digital out pin number on an arduino. Ex: 13
+        
+        Raises
+        ------
+        PyroError("DeviceLockedError")
+            Error to signal that the constuctor was not called and therefore the device is locked.
+        """
         try:
             self._activated
         except AttributeError:
@@ -41,6 +86,21 @@ class LAMP:
         self.board.digital[pin].write(1)
 
     def off(self,pin=13):
+        """"
+        Tell the arduino to turn a pin LOW.
+
+        Parameters
+        ----------
+        self.board : pyfirmata.Arduino
+            Oject that represents an arduino initialized in start(). Ex: pyfirmata.Arduino("COM4")
+        pin : int
+            Integer that represents the digital out pin number on an arduino. Ex: 13
+        
+        Raises
+        ------
+        PyroError("DeviceLockedError")
+            Error to signal that the constuctor was not called and therefore the device is locked.
+        """
         try:
             self._activated
         except AttributeError:
@@ -49,6 +109,21 @@ class LAMP:
         self.board.digital[pin].write(0)
 
     def close(self):
+        """"
+        Close the connection with the arduino.
+
+        Parameters
+        ----------
+        self.board : pyfirmata.Arduino
+            Oject that represents an arduino initialized in start(). Ex: pyfirmata.Arduino("COM4")
+        pin : int
+            Integer that represents the digital out pin number on an arduino. Ex: 13
+        
+        Raises
+        ------
+        PyroError("DeviceLockedError")
+            Error to signal that the constuctor was not called and therefore the device is locked.
+        """
         try:
             self._activated
         except AttributeError:
@@ -57,4 +132,7 @@ class LAMP:
         self.board.exit()
     
     def __del__(self):
+        """"
+        Function called when Proxy connection is lost.
+        """
         self.close()
