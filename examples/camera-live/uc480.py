94df56d1a9fad30f5d569c3d9a7e5784f963a1c1
hillda3141 <dah3141@byu.net>
2021-03-25 17:02:51 -0600

    changes made

--- examples/camera-live/uc480.py
+++ examples/camera-live/uc480.py
@@ -9,17 +9,36 @@ import time
 HEADERSIZE = 10
 BRIGHTNESS = 5
 PORT = 2222
-SER_NUMBER = 4103247225
+SER_NUMBER = 4103238947
+COLOR = False
 
 def bayer_convert(bayer):
-    dStack = np.clip((np.dstack(((0.469 + bayer*0.75 - (bayer^2)*0.003)*(BRIGHTNESS/5),(bayer*0.95)*(BRIGHTNESS/5),(0.389 + bayer*1.34 - (bayer^2)*0.004)*(BRIGHTNESS/5)))),0,255).astype('uint8')
+    if(COLOR):
+        ow = (bayer.shape[0]//4) * 4
+        oh = (bayer.shape[1]//4) * 4
+
+        R  = bayer[0::2, 0::2]
+        B  = bayer[1::2, 1::2]
+        G0 = bayer[0::2, 1::2]
+        G1 = bayer[1::2, 0::2]
+        G = G0[:oh,:ow]//2 + G1[:oh,:ow]//2
+        bayer_R = np.array(R, dtype=np.uint8).reshape(512, 640)
+        bayer_G = np.array(G, dtype=np.uint8).reshape(512, 640)
+        bayer_B = np.array(B, dtype=np.uint8).reshape(512, 640)
+
+        dStack = np.clip(np.dstack((bayer_B*(BRIGHTNESS/5),bayer_G*(BRIGHTNESS/5),bayer_R*(BRIGHTNESS/5))),0,255).astype('uint8')
+    else:
+        bayer_T = np.array(bayer, dtype=np.uint8).reshape(512, 640)
+        dStack = np.clip((np.dstack(((0.469 + bayer_T*0.75 - (bayer_T^2)*0.003)*(BRIGHTNESS/5),(bayer_T*0.95)*(BRIGHTNESS/5),(0.389 + bayer_T*1.34 - (bayer_T^2)*0.004)*(BRIGHTNESS/5)))),0,255).astype('uint8')
+    #dStack = np.clip(np.dstack((bayer,bayer,bayer)),0,255).astype('uint8')
     return dStack
 
 ns = locate_ns(host="camacholab.ee.byu.edu")
 cam = Proxy(ns.lookup("UC480"))
 
 cam.start(ser_no = SER_NUMBER, port = PORT)
-ip_address = cam.start_capture()
+ip_address = cam.start_capture(COLOR)
+print(ip_address)
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect((str(ip_address), PORT))
 
@@ -40,8 +59,7 @@ while(True):
                 imList = pickle.loads(msg)
                 break
                 
-    bayer = np.array(imList, dtype=np.uint8).reshape(512, 640)
-    dStack = bayer_convert(bayer)
+    dStack = bayer_convert(imList)
 
     cv2.imshow('scope',dStack)
     keyCode = cv2.waitKey(1)
