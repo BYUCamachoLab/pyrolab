3640c1daa5913273beb695709c0db951e35e9d03
hillda3141 <dah3141@byu.edu>
2021-04-16 10:54:00 -0600

    edit

--- examples/motion/kcube_list.py
+++ examples/motion/kcube_list.py
@@ -1,8 +1,7 @@
 import os
 os.add_dll_directory("C:\\Program Files\\Thorlabs\\Kinesis")
 
-from ctypes import c_int, c_double, byref, pointer
-import time
+from ctypes import c_int, c_double, c_short, byref, pointer
 from thorlabs_kinesis import kcube_dcservo as kc
 
 num = c_short(0)
