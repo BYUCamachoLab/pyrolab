38bf8e43465ff96743575de957672bb033ba7d5a
hillda3141 <dah3141@byu.edu>
2021-05-10 09:02:03 -0600

    example

--- examples/motion/nanomax300.py
+++ examples/motion/nanomax300.py
@@ -1,11 +1,25 @@
 import os
 os.add_dll_directory("C:\\Program Files\\Thorlabs\\Kinesis")
-
 from pyrolab.drivers.motion.nanomax300 import NanoMax300
-nm = NanoMax300("71874833", closed_loop=True)
 
+SER_NUM = 71874833
+
+print(f"connecting to {SER_NUM}...")
+nm = NanoMax300(str(SER_NUM), closed_loop=True)
+print("connected")
+print("zeroing all channels...")
 nm.zero()
+print("zeroed")
 x, y, z, = nm.get_position()
-nm.jog(1, 0.1)
+print(f"position of x: {x}")
+print(f"position of y: {y}")
+print(f"position of z: {z}")
+while(True):
+    channel = int(input("Input channel:"))
+    distance = float(input("Input jog distance:"))
+    nm.jog(channel,distance)
+    cond = input("Break?")
+    if(cond == "y" | cond == "yes"):
+        break
 
 nm.close()
