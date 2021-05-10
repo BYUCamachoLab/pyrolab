c812df6ed93726e2843c6782ab0529ab33664d24
davotrey <davotrey@gmail.com>
2021-05-10 09:15:09 -0600

    example for bcp303

--- examples/motion/nanomax300.py
+++ examples/motion/nanomax300.py
@@ -1,6 +1,7 @@
 import os
 os.add_dll_directory("C:\\Program Files\\Thorlabs\\Kinesis")
 from pyrolab.drivers.motion.nanomax300 import NanoMax300
+import time
 
 SER_NUM = 71874833
 
@@ -14,12 +15,21 @@ x, y, z, = nm.get_position()
 print(f"position of x: {x}")
 print(f"position of y: {y}")
 print(f"position of z: {z}")
+print("Input Jogging Parameters:")
 while(True):
     channel = int(input("Input channel:"))
     distance = float(input("Input jog distance:"))
+    print(f"jogging channel {channel} a distance of {distance}...")
     nm.jog(channel,distance)
+    time.sleep(1)
+    print("jog complete")
+    x, y, z, = nm.get_position()
+    print(f"new position of x: {x}")
+    print(f"new position of y: {y}")
+    print(f"new position of z: {z}")
     cond = input("Break?")
-    if(cond == "y" | cond == "yes"):
+    if((cond == "y") | (cond == "yes")):
         break
-
+print("closing nanomax...")
 nm.close()
+print("closed")
