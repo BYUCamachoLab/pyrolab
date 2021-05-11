54a1c365f824024c049c882f30d7b7da4769a137
David Andrew Hill <dah3141@et.byu.edu>
2021-05-11 08:13:00 -0600

    moved mask ability to client

--- examples/camera-live/uc480_local.py
+++ examples/camera-live/uc480_local.py
@@ -27,6 +27,25 @@ EXPOSURE = 65   #the exposure that the camera uses, this actually changes the
                 #raw camera data by changing the exposure time of the sensor
 SER_NUMBER = 4103247225 #serial number of the Thorlabs camera
 COLOR = True   #should the camera transmit colored data or grayscale?
+MASK_ON = True
+mask = np.arange(0)
+
+def _generate_mask(roi,num_cir=4):
+    max_rx = roi[0]/4
+    max_ry = roi[1]/4
+    max_r = min(max_rx,max_ry)
+
+    x, y = np.indices((int(roi[0]/2), int(roi[1]/2)))
+    array = np.zeros((int(roi[0]/2), int(roi[1]/2)))
+
+    rads = (max_r/num_cir*np.arange(1,num_cir+1)).astype(int)
+
+    for r in rads:
+        array = np.logical_or(array,np.abs(np.hypot(max_rx - x, max_ry - y)-r) < 0.5)
+    array = np.logical_or(array,x == round(max_rx))
+    array = np.logical_or(array,y == round(max_ry))
+    
+    return array.astype(int)
 
 def bayer_convert(bayer):
     """
@@ -37,7 +56,12 @@ def bayer_convert(bayer):
     bayer : numpy array
         this is the raw data that is recieved from the camera    
     """
+    global mask
+
     if(COLOR):  #if the data is in color, put the numpy array into BGR form
+        if(not mask.any()):
+            mask = _generate_mask(bayer.shape,4)
+
         frame_height = bayer.shape[0]//2
         frame_width = bayer.shape[1]//2
 
@@ -54,13 +78,27 @@ def bayer_convert(bayer):
         bayer_G = np.array(G, dtype=np.uint8).reshape(frame_height, frame_width)
         bayer_B = np.array(B, dtype=np.uint8).reshape(frame_height, frame_width)
 
+        if(MASK_ON == True):
+            idx=(mask==1)
+            bayer_R[idx]=255
+            bayer_G[idx]=255
+            bayer_B[idx]=255
+
         dStack = np.clip(np.dstack((bayer_B*(BRIGHTNESS/5),bayer_G*(BRIGHTNESS/5),
                          bayer_R*(BRIGHTNESS/5))),0,PIXEL_MAX).astype('uint8')
     else:   #if the data is grayscale, put it into BGR form
+        if(not mask.any()):
+            mask = _generate_mask((int(bayer.shape[0]*2),int(bayer.shape[1]*2)))
+
         frame_height = bayer.shape[0]
         frame_width = bayer.shape[1]
         bayer_T = np.array(bayer, dtype=np.uint8).reshape(frame_height,
                            frame_width)
+
+        if(MASK_ON == True):
+            idx=(mask==1)
+            bayer_T[idx]=255
+
         dStack = np.clip((np.dstack(((bayer_T)*(BRIGHTNESS/5),
                          (bayer_T)*(BRIGHTNESS/5),(bayer_T)*(BRIGHTNESS/5)))),0,
                          PIXEL_MAX).astype('uint8')
--- pyrolab/drivers/cameras/uc480.py
+++ pyrolab/drivers/cameras/uc480.py
@@ -118,8 +118,6 @@ class UC480:
         self.set_exposure(exposure)
         self.initialize_memory(pixelbytes)     
         self.port = port
-        self.mask_on = True
-        self._generate_mask(roi_shape)
         print("initialized")
 
     def _get_image(self):
@@ -146,10 +144,6 @@ class UC480:
 
             GRAY = R[:oh,:ow]//3 + B[:oh,:ow]//3 + (G0[:oh,:ow]//2 + G1[:oh,:ow]//2)//3
 
-            if(self.mask_on == True):
-                idx=(self.mask==1)
-                GRAY[idx]=255
-
             if(self.local == True):
                 return GRAY
             else:
@@ -157,16 +151,6 @@ class UC480:
                 msg = bytes(f'{len(msg):<{self.HEADERSIZE}}', "utf-8") + msg
                 return msg
         else:
-
-            if(self.mask_on == True):
-                large_mask = np.zeros((self.roi_shape[1],self.roi_shape[0]))
-                large_mask[0::2, 0::2]=self.mask
-                large_mask[1::2, 1::2]=self.mask
-                large_mask[0::2, 1::2]=self.mask
-                large_mask[1::2, 0::2]=self.mask
-                idx=(large_mask==1)
-                bayer[idx]=255
-
             if(self.local == True):
                 return bayer
             else:
@@ -203,24 +187,6 @@ class UC480:
                         break
             else:
                 break
-
-    def _generate_mask(self,roi):
-        max_rx = roi[0]/4
-        max_ry = roi[1]/4
-        max_r = min(max_rx,max_ry)
-
-        x, y = np.indices((int(roi[0]/2), int(roi[1]/2)))
-        array = np.zeros((int(roi[0]/2), int(roi[1]/2)))
-
-        rads = (max_r/4*np.arange(1,5)).astype(int)
-
-        for r in rads:
-            array = np.logical_or(array,np.abs(np.hypot(max_rx - x, max_ry - y)-r) < 0.5)
-        array = np.logical_or(array,x == round(max_rx))
-        array = np.logical_or(array,y == round(max_ry))
-        
-        self.mask = array.astype(int)
-
             
     def get_frame(self):
         """
