742aae61e42462e4bbbe1a8ac76b217e1a06a7ae
sequoiap <sequoiap4@gmail.com>
2021-06-01 11:42:45 -0600

    New inheritable Instrument functions for autoconnect

--- pyrolab/drivers/__init__.py
+++ pyrolab/drivers/__init__.py
@@ -13,15 +13,38 @@ Submodule containing drivers for each supported instrument type.
 
 import logging
 
+from pyrolab.server import expose
+from typing import Any, Dict, List
+
 
 log = logging.getLogger('pyrolab.drivers')
 
 
 class Instrument:
-    pass
+    def __init__(self) -> None:
+        self._autoconnect_params: Dict[str, Any] = {}
 
-class CustomProxy:
-    pass
+    @staticmethod
+    def detect_devices() -> List[Dict[str, Any]]:
+        raise NotImplementedError
 
-class CustomDaemon:
-    pass
+    def connect(self, **kwargs) -> bool:
+        """
+        Connects to instruments or services that require initialization.
+
+        Returns
+        -------
+        bool
+            True if connection was successful, False otherwise.
+        """
+        raise NotImplementedError
+
+    @expose
+    def autoconnect(self) -> None:
+        if not self._autoconnect_params:
+            raise Exception("No autoconnection parameters available")
+        else:
+            return self.connect(**self._autoconnect_params)
+
+    def close(self) -> None:
+        raise NotImplementedError
