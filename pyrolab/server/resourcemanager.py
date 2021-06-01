5b16246a5234ad81aef9f1c8d764d763820dc828
sequoiap <sequoiap4@gmail.com>
2021-06-01 11:50:36 -0600

    Resource manager soon to come, perhaps as implemented in example

--- pyrolab/server/resourcemanager.py
+++ pyrolab/server/resourcemanager.py
@@ -4,4 +4,20 @@
 # Licensed under the terms of the GNU GPLv3+ License
 # (see pyrolab/__init__.py for details)
 
+"""
+Server Resource Manager
+-----------------------
+
+The server resource manager handles the putting up and taking down of Daemons
+for various instruments. To increase computer speed and processing 
+capabilities in cases where multiple instruments are hosted from the same
+computer, each instrument is created in its own Daemon using the 
+Python ``multiprocessing`` module.
+"""
+
 import multiprocessing
+
+
+class ResourceConfiguration:
+    def __init__(self) -> None:
+        pass
