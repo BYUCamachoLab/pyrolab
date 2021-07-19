306a95a6f28efd1d6639a6d396d84ae38460b12b
sequoiap <sequoiap4@gmail.com>
2021-07-19 11:12:39 -0600

    Bumped version

--- pyrolab/__init__.py
+++ pyrolab/__init__.py
@@ -38,7 +38,7 @@ if sys.version_info < (3, 7, 0):
 __name__ = "PyroLab"
 __author__ = "CamachoLab"
 __copyright__ = "Copyright 2020, The PyroLab Project"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __license__ = "GPLv3+"
 __maintainer__ = "Sequoia Ploeg"
 __maintainer_email__ = "sequoia.ploeg@byu.edu"
