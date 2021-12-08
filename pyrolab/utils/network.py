bf75cdaca1ecdcd961e4efc1f40c13149f1d094a
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-07 19:31:49 -0700

    Docstring

--- pyrolab/utils/network.py
+++ pyrolab/utils/network.py
@@ -8,6 +8,14 @@ import socket
 
 
 def get_ip() -> str:
+    """
+    Get the IP address of the local machine.
+
+    Returns
+    -------
+    str
+        The IP address of the local machine.
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
