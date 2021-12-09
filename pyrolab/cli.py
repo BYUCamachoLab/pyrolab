2117f97ebeafd358245da70078891ef22d2429cb
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-08 19:24:58 -0700

    New command line interface

--- pyrolab/cli.py
+++ pyrolab/cli.py
@@ -1,7 +1,28 @@
-import click
-from yaml import load
+# -*- coding: utf-8 -*-
+#
+# Copyright © PyroLab Project Contributors
+# Licensed under the terms of the GNU GPLv3+ License
+# (see pyrolab/__init__.py for details)
 
-from pyrolab.nameserver import load_ns_configs
+import click
+
+from pyrolab.manager import DaemonManager
+from pyrolab.nameserver import start_ns_loop
+from pyrolab.configure import (
+    update_config,
+    reset_config,
+    load_ns_configs, 
+    load_server_configs, 
+    load_service_configs,
+    get_servers_used_by_services,
+    get_services_for_server,
+)
+from pyrolab.utils import bcolors
+
+
+def abort_if_false(ctx, param, value):
+    if not value:
+        ctx.abort()
 
 
 @click.group()
@@ -9,21 +30,152 @@ def cli():
     pass
 
 
-@cli.command()
-@click.option('--list-profiles', is_flag=True, help='List all installed profiles.')
-@click.option('-l', '--launch', default='default', help='Launch the specified nameserver profile ("default" started if none provided).')
+@cli.group()
+def launch(**kwargs):
+    """
+    Launch nameservers or a daemons.
+    """
+    pass
+
+
+@launch.command()
+@click.argument('nameserver', nargs=-1)
 def nameserver(**kwargs):
     """
-    Start the PyroLab Nameserver.
+    Launch a nameserver. If none specified, all are launched.
     """
-    if kwargs['list_profiles']:
-        print('Profiles:')
-        for profile in load_ns_configs().keys():
-            print(f'  {profile}')
-        return
-    if 'profile' in kwargs:
-        print(kwargs['profile'])
-        return
+    if len(kwargs['nameserver']) == 0:
+        print(f"Launching nameserver 'default'...")
+        nscfg = load_ns_configs()['default']
+    else:
+        print(f"Launching nameserver '{kwargs['nameserver'][0]}'...")
+        nscfg = load_ns_configs()[kwargs['nameserver'][0]]
+    nscfg.update_pyro_config()
+    start_ns_loop(nscfg)
+
+
+@launch.command()
+@click.argument('daemons', nargs=-1)
+def daemon(**kwargs):
+    """
+    Launch daemons and associated services. If none specified, all are launched.
+    """
+    print(f"Collecting daemons...")
+    if len(kwargs['daemons']) == 0 or kwargs['daemons'][0] == 'all':
+        which = load_server_configs().keys()
+    else:
+        which = [*kwargs['daemons']]
+    dm = DaemonManager.instance()
+    for daemon in which:
+        print(f"Launching daemon '{daemon}'...")
+        dm.launch(daemon)
+    dm.wait_for_interrupt()
+
+
+@cli.group()
+def config(**kwargs):
+    """
+    Update or reset configuration.
+    """
+    pass
+
+
+@config.command(no_args_is_help=True)
+@click.argument('filename', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+def update(**kwargs):
+    """
+    Use a configuration file to update PyroLab's configuration.
+    """
+    print('Updating pyrolab config file from {}...'.format(kwargs['filename']), end='')
+    update_config(kwargs['filename'])
+    print(bcolors.OKGREEN + 'done' + bcolors.ENDC)
+
+
+@config.command()
+@click.option('--yes', is_flag=True, callback=abort_if_false,
+              expose_value=False,
+              prompt='Are you sure you want to reset the configuration?')
+def reset(**kwargs):
+    """
+    Reset the pyrolab environment.
+    """
+    print('Resetting pyrolab config file (deleting custom configurations)...', end='')
+    reset_config()
+    print(bcolors.OKGREEN + 'done' + bcolors.ENDC)
+
+
+@cli.group()
+@click.option('-v', '--verbose', is_flag=True, help='Show verbose details.')
+@click.pass_context
+def ls(ctx, verbose):
+    """
+    List nameservers, daemons, services or servers.
+    """
+    ctx.ensure_object(dict)
+    ctx.obj['verbose'] = verbose
+
+
+@ls.command()
+@click.pass_context
+def nameservers(ctx, **kwargs):
+    """
+    List nameservers.
+    """
+    if not ctx.obj['verbose']:
+        print('Nameserver:')
+        for nscfg in load_ns_configs().keys():
+            print(f'  {nscfg}')
+    else:
+        print(f"Nameserver configuration (verbose):")
+        cfgs = load_ns_configs()
+            # cfgs = {kwargs['details']: load_ns_configs()[kwargs['details']]}
+        for nscfg in cfgs.keys():
+            print(f'  {nscfg}:')
+            for k, v in cfgs[nscfg].to_dict().items():
+                print(f'    {k}: {v}')
+
+            
+@ls.command()
+@click.pass_context
+def daemons(ctx, **kwargs):
+    """
+    List daemons.
+    """
+    if not ctx.obj['verbose']:
+        print('Daemons:')
+        for daemon in load_server_configs().keys():
+            print(f'  {daemon}')
+    else:
+        print(f"Daemon configuration (verbose):")
+        cfgs = load_server_configs()
+        for daemon in cfgs.keys():
+            print(f'  {daemon}:')
+            for k, v in cfgs[daemon].to_dict().items():
+                print(f'    {k}: {v}')
+
+
+@ls.command()
+@click.pass_context
+@click.option('-d', '--daemon', help='List services specific to a daemon.')
+def services(ctx, **kwargs):
+    """
+    List services.
+    """
+    if not kwargs['daemon']:
+        services = load_service_configs()
+    else:
+        services = get_services_for_server(kwargs['daemon'])
+
+    if not ctx.obj['verbose']:
+        print('Services:')
+        for service in services.keys():
+            print(f'  {service}')
+    else:
+        print(f"Service configuration (verbose):")
+        for service in services.keys():
+            print(f'  {service}:')
+            for k, v in services[service].to_dict().items():
+                print(f'    {k}: {v}')
 
 
 if __name__ == "__main__":
