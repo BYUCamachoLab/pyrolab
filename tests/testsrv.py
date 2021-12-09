2998ca7abef8d3bea92611abe5cd7e5af1ba7721
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-08 19:25:37 -0700

    Test files

--- tests/testsrv.py
+++ tests/testsrv.py
@@ -1,15 +1,15 @@
+from multiprocessing import Lock
 from pyrolab.api import locate_ns, behavior, change_behavior
 from pyrolab.drivers.sample import SampleService
-from pyrolab.server import LockableDaemon, create_lockable
+from pyrolab.daemon import LockableDaemon
 
 
 daemon = LockableDaemon()
 ns = locate_ns(host="localhost")
-SS = create_lockable(SampleService)
-SS = change_behavior(SS, instance_mode="single")
+SS = LockableDaemon.prepare_class(SampleService)
+SS = SS.set_behavior(instance_mode="single")
 print(SS._pyroInstancing)
 import sys; sys.exit()
-# SS._pyroInstancing = ("single", None)
 # uri = daemon.register(SS)
 # ns.register("test.SampleService", uri, metadata={"You can put lists of strings here!"})
 ns.register("test.SampleService", daemon.register(SS), metadata={"You can put lists of strings here!"})
