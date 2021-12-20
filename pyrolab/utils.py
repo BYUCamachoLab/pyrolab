a1fd3fe1b2aceef85a27830c9d8c9e2dec82cff4
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-20 15:03:36 -0700

    Utils collapsed into one module

--- pyrolab/utils.py
+++ pyrolab/utils.py
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © PyroLab Project Contributors
+# Licensed under the terms of the GNU GPLv3+ License
+# (see pyrolab/__init__.py for details)
+
+import socket
 import secrets
 import pkg_resources
+
+
+def get_ip() -> str:
+    """
+    Get the IP address of the local machine.
+
+    Returns
+    -------
+    str
+        The IP address of the local machine.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
 
 class bcolors:
@@ -13,7 +36,8 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def generate_random_name(count=3) -> str:
+
+def generate_random_name(count: int=3) -> str:
     """
     Concatenates ``count`` random words as a hyphenated string.
 
