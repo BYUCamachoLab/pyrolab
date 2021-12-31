7631f5faa4f91ccd71276528bb61ef060b9e00c2
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-31 13:54:33 -0700

    Removed unnecessary terminal colorprinting class

--- pyrolab/utils.py
+++ pyrolab/utils.py
@@ -25,19 +25,6 @@ def get_ip() -> str:
     return ip
 
 
-# TODO: Remove
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 def generate_random_name(count: int=3) -> str:
     """
     Concatenates ``count`` random words as a hyphenated string.
