e956a1377d9a847df4fff4d6a83c3a4210867168
Sequoia Ploeg <sequoiap4@gmail.com>
2021-12-31 13:16:54 -0700

    A convenience script for listing ns registrations

---
+++ scripts/list_ns.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from tabulate import tabulate
+from pyrolab.configure import NameServerConfiguration
+from pyrolab.api import locate_ns
+
+
+HOST = "camacholab.ee.byu.edu"
+# HOST = "localhost"
+PORT = 9090
+
+ns = locate_ns(host=HOST, port=PORT)
+services = ns.list(return_metadata=True)
+
+listing = []
+for k, v in services.items():
+    name = k
+    uri, description = v
+    listing.append([name, uri, ': '.join(description)])
+
+print(tabulate(listing, headers=['NAME', 'URI', 'DESCRIPTION']))
