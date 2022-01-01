5b3d922eb993e7a786e90ba75cea22d168c1cf95
sequoiap <sequoiap4@gmail.com>
2022-01-01 02:20:14 -0700

    Configure test directory

--- tests/test_code_interface.py
+++ tests/test_code_interface.py
@@ -1,29 +1,29 @@
-import time
+# import time
 
-from pyrolab.manager import ProcessManager
-from pyrolab.api import locate_ns, Proxy, GlobalConfiguration
+# from pyrolab.manager import ProcessManager
+# from pyrolab.api import locate_ns, Proxy, GlobalConfiguration
 
-GLOBAL_CONFIG = GlobalConfiguration.instance()
-GLOBAL_CONFIG.load_config("tests/devconfig.yaml")
-ns_names = list(GLOBAL_CONFIG.get_config().nameservers.keys())
-pm = ProcessManager.instance()
-print("nameservers:", ns_names)
+# GLOBAL_CONFIG = GlobalConfiguration.instance()
+# GLOBAL_CONFIG.load_config("tests/devconfig.yaml")
+# ns_names = list(GLOBAL_CONFIG.get_config().nameservers.keys())
+# pm = ProcessManager.instance()
+# print("nameservers:", ns_names)
 
-pm.launch_nameserver('default')
-pm.launch_daemon('lockable')
+# pm.launch_nameserver('default')
+# pm.launch_daemon('lockable')
 
-time.sleep(5)
+# time.sleep(5)
 
-ns = locate_ns()
-services = list(ns.list().keys())[1:]
+# ns = locate_ns()
+# services = list(ns.list().keys())[1:]
 
-p1 = Proxy(ns.lookup(services[0]))
-resp = p1.echo("hi there")
-assert resp == "SERVER RECEIVED: hi there"
+# p1 = Proxy(ns.lookup(services[0]))
+# resp = p1.echo("hi there")
+# assert resp == "SERVER RECEIVED: hi there"
 
-p2 = Proxy(ns.lookup(services[1]))
-resp = p2.autoconnect()
-assert resp == True
+# p2 = Proxy(ns.lookup(services[1]))
+# resp = p2.autoconnect()
+# assert resp == True
 
-pm.shutdown_daemon('lockable')
-pm.shutdown_nameserver('default')
+# pm.shutdown_daemon('lockable')
+# pm.shutdown_nameserver('default')
