bd1e4585a3a6e68f0b2b3eeed2e2aa46cfd06411
sequoiap <sequoiap4@gmail.com>
2022-01-05 02:27:12 -0700

    Added example for thorcam client

--- examples/thorcam/use.py
+++ examples/thorcam/use.py
@@ -1,6 +1,6 @@
 import time
 
-from pyrolab.drivers.cameras.uc480 import UC480Client
+from pyrolab.drivers.cameras.thorcam import ThorCamClient
 
 from pyrolab.api import NameServerConfiguration
 import numpy as np
@@ -9,16 +9,22 @@ import cv2
 nscfg = NameServerConfiguration(host="camacholab.ee.byu.edu")
 nscfg.update_pyro_config()
 
-uc480 = UC480Client()
+uc480 = ThorCamClient()
+print("connecting")
 uc480.connect("asgard.rocket")
+print("connected")
 
-uc480.start_video()
+uc480.start_stream()
+print("video started")
 time.sleep(3)
+print("let's get to it")
 while(True):
-    cv2.imshow('UC480',uc480.get_frame())  #paint the image to the cv2 window
+    frame = uc480.get_frame()
+    print(f"got frame, {frame.shape}")
+    cv2.imshow('UC480',frame)  #paint the image to the cv2 window
     keyCode = cv2.waitKey(30)
 
     if cv2.getWindowProperty('UC480',cv2.WND_PROP_VISIBLE) < 1:   
         break
 
-uc480.end_video()
+uc480.end_stream()
