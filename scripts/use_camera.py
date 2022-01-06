09c8bcb58ae3770a881cdf2ce08ef76778b36c61
hillda3141 <dah3141@byu.net>
2022-01-06 10:12:45 -0700

    new use case for camera roi

--- scripts/use_camera.py
+++ scripts/use_camera.py
@@ -11,10 +11,15 @@ nscfg.update_pyro_config()
 
 uc480 = ThorCamClient()
 uc480.connect("asgard.rocket")
-uc480.color = False
+# uc480.color = False
 print(uc480.color)
 print(uc480.exposure)
+uc480.brightness = 10
 print(uc480.brightness)
+uc480.roi_shape = [512,640]
+uc480.roi_pos = [0,0]
+print(uc480.roi_shape)
+print(uc480.roi_pos)
 
 uc480.start_stream()
 time.sleep(1)
@@ -27,7 +32,6 @@ while(True):
         break
     
     count = count + 1
-    if count == 100:
-        uc480.exposure = 20
+        
 
 uc480.end_stream()
