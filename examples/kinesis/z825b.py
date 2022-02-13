c07cfc741562e1e19aa9e3f4c4129e249b94b1a5
sequoiap <sequoiap4@gmail.com>
2022-02-12 19:20:21 -0700

    Clean up examples, make them generic

--- examples/kinesis/z825b.py
+++ examples/kinesis/z825b.py
@@ -1,14 +1,26 @@
-import time
+# -*- coding: utf-8 -*-
+#
+# Copyright © PyroLab Project Contributors
+# Licensed under the terms of the GNU GPLv3+ License
+# (see pyrolab/__init__.py for details)
+
+"""
+Kinesis KCube Driver Example
+============================
+
+This example demonstrates the use of the Thorlabs KCube DC Servo controlling a 
+Z825B translational stage using PyroLab. 
+"""
 
 from pyrolab.api import NameServerConfiguration, Proxy, locate_ns
-import numpy as np
 
 
-nscfg = NameServerConfiguration(host="camacholab.ee.byu.edu")
+nscfg = NameServerConfiguration(host="yourdomain.com")
 nscfg.update_pyro_config()
 
+# Be considerate; don't stay connected to the nameserver too long.
 with locate_ns() as ns:
-    motor = Proxy(ns.lookup("asgard.hulk"))
+    motor = Proxy(ns.lookup("Z825B_PyroName"))
 
 motor.autoconnect()
 print(motor.get_position())
