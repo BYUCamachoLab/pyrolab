09154cb181fd45717ed97b24b63838462750f63f
Sequoia Ploeg <sequoia.ploeg@nokia.com>
2023-02-25 00:34:46 -0700

    Add check for lock acquisition error

--- src/pyromonitor/lister.py
+++ src/pyromonitor/lister.py
@@ -1,19 +1,21 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 
 from pyrolab.api import locate_ns, Proxy
+from pyrolab.errors import LockAcquisitionError
 
 
-bp = Blueprint('lister', __name__)#, url_prefix='/lister')
+bp = Blueprint('lister', __name__)
 
 ns = locate_ns("camacholab.ee.byu.edu")
 instruments: dict = ns.list(return_metadata=True)
 instruments.pop("Pyro.NameServer") # no need to display the nameserver
 for inst, vals in instruments.items():
-    # TODO: Check if it's locked
     try:
         p = Proxy(ns.lookup(inst))
         p.autoconnect()
         instruments[inst] = *vals, "Available"
+    except LockAcquisitionError as e:
+        instruments[inst] = *vals, "Locked"
     except:
         instruments[inst] = *vals, "Unavailable"
 
