f58112bc30078eaf259b0901d097c5f28188d8f8
Sequoia Ploeg <sequoia.ploeg@nokia.com>
2023-02-28 10:39:18 -0700

    Updated build requirements

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ from datetime import datetime
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
