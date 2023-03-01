5747240e1db1dc65592c1100008ac5698733216b
Sequoia Ploeg <sequoia.ploeg@nokia.com>
2023-02-28 20:33:28 -0700

    Fixed locking example

--- examples/locking/nameserver.py
+++ examples/locking/nameserver.py
@@ -12,7 +12,8 @@
 """
 
 from pyrolab.api import start_ns_loop
+from pyrolab.configure import NameServerConfiguration
 
 # Let's run our nameserver! It will use the default profile, which provides
 # sensible defaults--running on 'localhost', default port 9090, etc.
-start_ns_loop()
+start_ns_loop(NameServerConfiguration())
